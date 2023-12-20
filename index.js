790649f2be1eb6ac834d7032d7ce6fa89572cf06
Kearabetswe <keabetswe36110@example.com>
2023-12-20 23:31:55 +0200

    my perfect design

---
+++ index.js
@@ -0,0 +1,15 @@
+// const myApi = "https://api.openweathermap.org/data/2.5/weather?lat=-25.7478676&lon=28.2292712&appid=27d67740c13712be69d1960c02c030eb";
+// fetch(myApi)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//     })
+//     .catch(error => {
+//       console.error('Error:', error);
+//     });
+
